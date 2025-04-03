b5495666676c2c7f25ffc07e4a7dd0bf51866f45
Marcos Vinicius <marcosvinicius.lucena27@gmail.com>
2025-04-03 18:56:22 -0300

    correcoes

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Servir arquivos estáticos da pasta atual
+app.use(express.static(__dirname));
+
+// Definir rota principal para servir o index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
 // Defina os valores diretamente no código
 const TELEGRAM_BOT_TOKEN = "8145134129:AAGbRgrOOnc0_3_eCVawdvNBqHrDbY_EMhc";
 const TELEGRAM_CHAT_ID = "-4725079122";
